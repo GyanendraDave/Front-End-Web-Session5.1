dc9c52e947e596003deecf83dd102d2c606cbfad
GyanendraDave <gyandreamworld7868@gmail.com>
2017-07-18 01:55:43 +0530

    Add files via upload

---
+++ assignment5.1/js/app.js
@@ -0,0 +1,10 @@
+var name = "Gyan",
+    age = 30,
+    dateofbirth = "3 October 1987",
+    birthplace = "Sayla";
+
+console.log("Name" + " = " + name + "," + " " +
+    "Age" + " = " + age + "," + " " +
+    "Date Of Birth" + " = " + dateofbirth + "," + " " +
+    "Place of Birth" + " = " + birthplace
+);
